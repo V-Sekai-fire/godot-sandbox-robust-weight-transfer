96404b58cb3b3a236136dcd51abe545f07754649
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-11-16 23:12:43 -0800

    Try to recreate the failing case.

--- robust_weight_transfer/core/weight_transfer.gd
+++ robust_weight_transfer/core/weight_transfer.gd
@@ -7,12 +7,6 @@ func _ready() -> void:
 	set_program(program)
 	set_redirect_stdout(_print)
 	vmcall("run_tests")
-	var quad_mesh_source = QuadMesh.new()
-	print(quad_mesh_source.get_surface_count())
-	var quad_mesh_target = QuadMesh.new()
-	print(quad_mesh_target.get_surface_count())
-	var result = vmcall("find_matches_closest_surface_mesh", quad_mesh_source, quad_mesh_target)
-	print(result)
 	
 func _print(line) -> void:
 	print(line)
