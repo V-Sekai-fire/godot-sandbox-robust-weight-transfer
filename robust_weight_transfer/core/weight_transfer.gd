f72c9b128e5a2eb5b7d1eddfc34fb3e2ac78af18
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-11-17 01:12:26 -0800

    Restore the last working.

--- robust_weight_transfer/core/weight_transfer.gd
+++ robust_weight_transfer/core/weight_transfer.gd
@@ -7,9 +7,6 @@ func _ready() -> void:
 	set_program(program)
 	set_redirect_stdout(_print)
 	vmcall("run_tests")
-	var array: Array = []
-	vmcall("find_matches_closest_surface_mesh", QuadMesh.new(), BoxMesh.new(), array)
-
 
 func _print(line) -> void:
 	print(line)
